19590855cda8778d4450a541aa3901b8045aea8c
Ivan Jovanovic <ij.smartnet@gmail.com>
2017-04-21 08:46:30 +0200

    AT-301 updated syntax

--- tangere/addon/tangere-hint-class-data.js
+++ tangere/addon/tangere-hint-class-data.js
@@ -3,41 +3,31 @@
 })(function(CodeMirror){
   "use strict";
 
-  if (CodeMirror.tangereHint == undefined) {
-    CodeMirror.tangereHint = {
-      classes: []
-    };
-  } else {
-    CodeMirror.tangereHint.classes = [];
-  }
-
-  CodeMirror.tangereHint.classes.push({
+  var classes = [{
     class: "tangere-class1",
     description: "class1 description"
-  });
-
-  CodeMirror.tangereHint.classes.push({
+  }, {
     class: "empty-desc-class",
     description: ""
-  });
-
-  CodeMirror.tangereHint.classes.push({
+  }, {
     class: "tangere-class3",
     description: "class3 description"
-  });
-
-  CodeMirror.tangereHint.classes.push({
+  }, {
     class: "null-desc-class",
     description: null
-  });
-
-  CodeMirror.tangereHint.classes.push({
+  }, {
     class: "undefined-desc-class",
-  });
-
-  CodeMirror.tangereHint.classes.push({
+  }, {
     class: "undefined2-desc-class",
     description: undefined
-  });  
+  }];
 
+  if (CodeMirror.tangereHint == undefined) {
+    CodeMirror.tangereHint = {
+      classes: classes
+    };
+  } else {
+    CodeMirror.tangereHint.classes = classes;
+  }
+  
 });
